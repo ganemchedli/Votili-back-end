882e93a4dca10e27db8a9b03b8b36d9ef583a85a
ganemchedli <ganemchedli@gmail.com>
2023-03-21 00:09:40 +0100

    config server created

---
+++ authentification/src/main/java/com/example/authentification/AuthentificationApplication.java
@@ -0,0 +1,13 @@
+package com.example.authentification;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AuthentificationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AuthentificationApplication.class, args);
+	}
+
+}
