2b8703e45c85d4298cee2e93d14f09ae6355415a
ganemchedli <ganemchedli@gmail.com>
2023-03-22 17:38:27 +0100

    Inscription created: error in creating beans

---
+++ inscription/src/main/java/com/example/inscription/InscriptionApplication.java
@@ -0,0 +1,13 @@
+package com.example.inscription;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InscriptionApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(InscriptionApplication.class, args);
+	}
+
+}
