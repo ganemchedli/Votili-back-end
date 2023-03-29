6e69430f04e5c71b4afdbf15fdc18ff1fdba6a49
ganemchedli <ganemchedli@gmail.com>
2023-03-29 08:32:50 +0100

    Creating the sql query

--- authentification/src/main/java/com/example/authentification/service/UserService.java
+++ authentification/src/main/java/com/example/authentification/service/UserService.java
@@ -5,6 +5,7 @@ import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
+import java.sql.*;
 
 import java.util.ArrayList;
 @Service
@@ -12,7 +13,18 @@ public class UserService implements UserDetailsService {
     @Override
     public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
         //logic to get the user from the database
-
+        try(
+                Connection conn = DriverManager.getConnection(
+                        "jdbc:mysql://localhost:3306/ebookshop?allowPublicKeyRetrieval=true&useSSL=false&serverTimezone=UTC",
+                        "myuser", "xxxx");
+                Statement stmt = conn.createStatement();
+        ){
+            String strSelect = "select title, price, qty from books";
+            ResultSet rset = stmt.executeQuery(strSelect);
+        }
+        catch(SQLException ex) {
+            ex.printStackTrace();
+        }
         return new User("admin","password",new ArrayList<>());
     }
 }
