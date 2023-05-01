077dc3ee771ffbda10dfb4a0e5b748bcadd8c1f0
unknown <ichrak.benhsinne@etudiant-enit.utm.tn>
2023-05-01 22:38:52 +0200

    added method sending mail with attachement

--- envoi-courriel-service/src/main/java/com/example/envoicourrielservice/service/NotificationService.java
+++ envoi-courriel-service/src/main/java/com/example/envoicourrielservice/service/NotificationService.java
@@ -25,4 +25,16 @@ public class NotificationService {
 		 mail.setText("This is a microservice that I created for our application to invite people to vote or participate in specific elections by sending them an email. It look worke very well !! yeyyyyyyy XD.");
 		 javaMailSender.send(mail);
 	}
+	public void sendNotificationWithAttachment(User user)throws Exception {
+        MimeMessage msg = javaMailSender.createMimeMessage();
+
+        MimeMessageHelper helper = new MimeMessageHelper(msg, true);
+        helper.setTo(user.getEmailAdress());
+        helper.setFrom("ichrakbenhsinne2@gmail.com");
+        helper.setSubject("Surprise");
+        helper.setText("You have been invited to participate in this election. Please click on the link below to cast your vote");
+        helper.addAttachment("logo.png", new ClassPathResource("logo.png"));
+        helper.setText("https:4200//inscriptionelection");
+        javaMailSender.send(msg);
+    }
 }
